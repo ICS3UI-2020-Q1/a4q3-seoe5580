436fdf0ad006e5c174f2d007042beaa1402b8e3f
seoe5580 <seoe5580@wrdsb.ca>
2020-10-03 00:50:57 +0000

    Done assignment

--- Main.java
+++ Main.java
@@ -1,6 +1,7 @@
+import java.util.Scanner;
 /**
- *
- * @author 
+ * Calculates the sum from one to user's number
+ * @author Eric Seo
  */
 public class Main {
 
@@ -9,7 +10,26 @@ public class Main {
    * @param args the command line arguments
    */
   public static void main(String[] args) {
-    // TODO code application logic here
+    // Creates the scanner for user input
+    Scanner input = new Scanner(System.in);
+
+    // Asks the user to input a number
+    System.out.println("Please enter an integer to count down to");
     
+    // Gets the integer from the user
+    int num = input.nextInt();
+
+    // declares variables
+    int count = 0;
+    int sum = 0;
+
+    // Calculates the math for the user
+    while(count < num){
+      count = count + 1;
+      sum = sum + count;
+    }
+    // tells the user the answer
+    System.out.println("The sum from 1 - " + num + "is " + sum);
+
   }
 }
